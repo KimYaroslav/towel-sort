ddc31369f0e30df6b2b5e94ef319ff17c47ad066
KimYaroslav <Korean_mafia_01@mail.ru>
2023-02-16 10:13:25 +0600

    feat: add task solution in js file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,27 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let resultArr = [];
+
+  if (arguments[0] == null) {return []}
+
+  else if (matrix.length === 0) {return []}
+
+  else {
+
+  for (i = 0; i < matrix.length; i++) {
+    if (i % 2 === 0) {
+      resultArr.push(matrix[i].join(','));
+    } else {resultArr.push(matrix[i].reverse().join(','))};
+  };
+
+  resultArr = resultArr.join(',').split(',');
+
+  resultArr.forEach(element => {
+    +element;
+  });
+
+  return resultArr;
+  }
+
 }
